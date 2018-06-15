13c49c32aafde5cfb785285f1cc5b1e834e1f1e7
Shashank Agrawal <shagrawal99@gmail.com>
2018-06-15 15:57:56 +0530

    Added some docs & implemented end & isEnabled method for iOS

---
+++ README.md
@@ -0,0 +1,15 @@
+# Cordova Guided Access Mode for iOS
+
+## Usage
+
+```
+cordova.plugins.WPGuidedAccess.start();
+cordova.plugins.WPGuidedAccess.end();
+cordova.plugins.WPGuidedAccess.isStarted();
+```
+
+## Research & Findings
+
+### iOS
+
+1. https://developer.apple.com/documentation/uikit/1615186-uiaccessibilityrequestguidedacce?language=objc
--- src/ios/WPGuidedAccess.m
+++ src/ios/WPGuidedAccess.m
@@ -4,13 +4,29 @@
 @implementation WPGuidedAccessMode
 
 - (void)start:(CDVInvokedUrlCommand*)command {
-    BOOL enableFoo = true;
+    BOOL enable = true;
 
-    // https://developer.apple.com/documentation/uikit/1615186-uiaccessibilityrequestguidedacce?language=objc
-    UIAccessibilityRequestGuidedAccessSession(enableFoo, ^(BOOL didSucceed) {
-        NSLog(@"Animation over..");
+    UIAccessibilityRequestGuidedAccessSession(enable, ^(BOOL didSucceed) {
+        NSLog(@"Guided Access Started");
         NSLog(didSucceed ? @"Yes" : @"No");
     });
+}
+
+- (void)end:(CDVInvokedUrlCommand*)command {
+    BOOL enable = false;
+
+    UIAccessibilityRequestGuidedAccessSession(enable, ^(BOOL didSucceed) {
+        NSLog(@"Guided Access Stopped");
+        NSLog(didSucceed ? @"Yes" : @"No");
+    });
+}
+
+- (void)isEnabled:(CDVInvokedUrlCommand*)command {
+    BOOL guidedAccessEnabled = UIAccessibilityIsGuidedAccessEnabled();
+    
+    CDVPluginResult* result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsBool:guidedAccessEnabled];
+    
+    [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
 }
 
 @end
