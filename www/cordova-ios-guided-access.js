0b1d1ceda94df468fd12c49752cd8522c4f213fd
Shashank Agrawal <shagrawal99@gmail.com>
2018-06-15 14:49:36 +0530

    Changing Cordova plugin JS with sensible names

--- www/cordova-ios-guided-access.js
+++ www/cordova-ios-guided-access.js
@@ -1,5 +1,9 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function (arg0, success, error) {
-    exec(success, error, 'WPGuidedAccessMode', 'start', [arg0]);
+var WPGuidedAccessMode = {
+    start: function(success, error) {
+        exec(success, error, 'WPGuidedAccessMode', 'start', []);
+    }
 };
+
+module.exports = WPGuidedAccessMode;
